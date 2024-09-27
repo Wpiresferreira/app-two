47ab94570c24fdadc45652e131013b5edd5237fc
Wagner Pires Ferreira <wagner_pires@icloud.com>
2024-09-27 15:20:43 -0600

    Add event listeners. DblClick on photo and hover the details.

--- src/components/ProfileDetails.jsx
+++ src/components/ProfileDetails.jsx
@@ -1,7 +1,20 @@
+import { useState } from "react"
+
 export default function ProfileDetails(props){
 
+const [style, setStyle] = useState('')
+
+    const handleMouseEnter = ()=>{
+        // alert('text')
+        setStyle('font-bold')
+    }
+    const handleMouseLeave = ()=>{
+        setStyle('')
+    }
+
+
     return (
-        <div className="p-3 flex flex-col justify-around">
+        <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className={`${style} p-3 flex flex-col justify-around`}>
         <div>Age: {props.age}</div>
         <div>Department: {props.department}</div>
         <div>Hobby: {props.hobby}</div>
--- src/components/ProfileHeader.jsx
+++ src/components/ProfileHeader.jsx
@@ -1,8 +1,23 @@
-export default function ProfileHeader({url, name}) {
+import { useState } from "react";
+
+export default function ProfileHeader({ url, name }) {
+  const [roundedPhoto, setRoundedPhoto] = useState("rounded-full");
+
+  const handleDoubleClick = () => {
+    if (roundedPhoto === "rounded-full") {
+      setRoundedPhoto("rounded-md");
+    } else {
+      setRoundedPhoto("rounded-full");
+    }
+  };
+
   return (
     <>
       <div className="flex-column items-center justify-center p-4">
-        <div className="w-[150px] rounded-full overflow-hidden">
+        <div
+          onDoubleClick={handleDoubleClick}
+          className={`${roundedPhoto}  w-[150px] rounded-full overflow-hidden`}
+        >
           <img src={url} alt="Avatar" />
         </div>
         <div className="text-center">{name}</div>
